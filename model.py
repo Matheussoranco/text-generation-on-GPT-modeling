ecc2b2668ae5b070c9ba285fead431c7d5947da4
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2024-11-30 17:17:48 -0300

    Created mask and fixed Tipo. Keras not importing

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["KERAS_BACKEND"] = "tesnorflow"
+os.environ["KERAS_BACKEND"] = "tensorflow"
 
 import keras
 from keras import layers
@@ -12,3 +12,14 @@ import random
 import tensorflow
 import tensorflow.data as tf_data
 import flow.strings as tf_strings
+
+def casual_attention_mask(batch_size, n_dest, n_src, dtype):
+    i = ops.arange(n_dest)[:, None]
+    j = ops.arange(n_src)
+    m = i >= j - n_src + n_dest
+    mask = ops.cast(m, dtype)
+    mask = ops.reshape(mask, [1, n_dest, n_src])
+    mult = ops.concatenate(
+        [ops.expand_dims(batch_size, -1, ops.convert_to_tensor([1, 1]))], 0
+    )
+    return ops.tile(mask, mult)
