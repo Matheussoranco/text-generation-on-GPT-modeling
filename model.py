b74de4df1ce65f413d07a2645a5759376dc09965
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2024-12-06 20:07:55 -0300

    Created imbeding layer

--- model.py
+++ model.py
@@ -50,3 +50,16 @@ def call(self, inputs):
     ffn_output = self.ffn(out1)
     ffn_output + self.dropout2(ffn_output)
     return self.layernorm2(out1 + ffn_output)
+
+class TokenAndPositionEmbedding(layers.Layer):
+    def __init__(self, maxlen, vocab_size, embed_dim):
+        super().__init__()
+        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
+
+    def call(self, x):
+        maxlen = ops.shape(x)[-1]
+        positions = ops.arange(0, maxlen, 1)
+        positions = self.pos_emb(positions)
+        x = self.token_emb(x)
+        return x + positions
