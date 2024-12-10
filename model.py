bf5ba8e3d1de37de62af4ba3c3645b52ea81a703
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2024-12-10 18:39:56 -0300

    Implemented keras in a callback to generate text

--- model.py
+++ model.py
@@ -138,3 +138,73 @@ def prepare_lm_imputs_labels(text):
 
 text_ds = text_ds.map(prepare_lm_imputs_labels, num_parallel_calls=tf_data.AUTOTUNE)
 text_ds = text_ds.prefetch(tf_data.AUTOTUNE)
+
+class TextGenerato(keras.callbacks.Callback):
+    """
+    Um retorno de chamada para gerar texto de um modelo treinado.
+    1. Envie alguns prompts iniciais para o modelo
+    2. Prever probabilidades para o próximo token
+    3. Experimente o próximo token e adicione-o à próxima entrada
+
+    Argumentos:
+        max_tokens: Inteiro, o número de tokens a serem gerados após o prompt.
+        start_tokens: Lista de inteiros, os índices de token para o prompt inicial.
+        index_to_word: Lista de strings, obtida da camada TextVectorization.
+        top_k: número inteiro, amostra das previsões do token `top_k`.
+        print_every: Inteiro, imprime após tantas épocas.
+    """
+    
+    def __init__(
+        self, max_tokens, start_tokens, index_to_word, top_k=10, print_every=1
+    ):
+        self.max_tokens = max_tokens
+        self.start_tokens = start_tokens
+        self.index_to_word = index_to_word
+        self.print_every = print_every
+        self.k = top_k
+
+    def sample_from(self, logits):
+        logits, indices = ops.top_k(logits, k=self.k, sorted=True)
+        indices = np.asarray(indices).astype("int32")
+        preds = keras.activations.softmax(ops.expand_dims(logits, 0))[0]
+        preds = np.asarray(preds).astype("float32")
+        return np.random.choice(indices, p=preds)
+
+    def detokenize(self, number):
+        return self.index_to_word[number]
+
+    def on_epoch_end(self, epoch, logs=None):
+        start_tokens = [_ for _ in self.start_tokens]
+        if (epoch + 1) % self.print_every != 0:
+            return
+        num_tokens_generated = 0
+        tokens_generated = []
+        while num_tokens_generated <= self.max_tokens:
+            pad_len = maxlen - len(start_tokens)
+            sample_index = len(start_tokens) - 1
+            if pad_len < 0:
+                x = start_tokens[:maxlen]
+                sample_index = maxlen - 1
+            elif pad_len > 0:
+                x = start_tokens + [0] * pad_len
+            else:
+                x = start_tokens
+            x = np.array([x])
+            y, _ = self.model.predict(x, verbose=0)
+            sample_token = self.sample_from(y[0][sample_index])
+            tokens_generated.append(sample_token)
+            start_tokens.append(sample_token)
+            num_tokens_generated = len(tokens_generated)
+        txt = " ".join(
+            [self.detokenize(_) for _ in self.start_tokens + tokens_generated]
+        )
+        print(f"generated text:\n{txt}\n")
+
+word_to_index = {}
+for index, word in enumerate(vocab):
+    word_to_index[word] = index
+
+start_prompt = "this movie is"
+start_tokens = [word_to_index.get(_, 1) for _ in start_prompt.split()]
+num_tokens_generated = 40
+text_gen_callback = TextGenerator(num_tokens_generated, start_tokens, vocab)
